d5a3ddf12fb0ea06fe7ced1506affab118bbb719
thuy tran van <thuytranvan.gl@gmail.com>
2023-09-30 18:17:47 +0700

    v2

--- taochuky.py
+++ taochuky.py
@@ -38,19 +38,12 @@ def randomword(length):
    letters = string.ascii_lowercase
    return ''.join(random.choice(letters) for i in range(length))
 
-def clone_node(node):
-    newn = node.copy()
-    newn.set('id', "")
-    return newn
-
 class TaoChuKyExtension(inkex.EffectExtension, inkex.base.TempDirMixin):
     """EffectExtension to fill selected objects red"""
 
     def add_arguments(self, pars):
-        pars.add_argument("--demo-dpi", type=int, default=100, help="DPI for demo file")
         pars.add_argument("--dpi", type=int, default=700, help="DPI")
         pars.add_argument("--filename", type=str, help="File name")
-        pars.add_argument("--export-type", type=str, default="demo", help="Export type: demo|done")
 
     def effect(self):
         start = time.time()
@@ -64,11 +57,19 @@ class TaoChuKyExtension(inkex.EffectExtension, inkex.base.TempDirMixin):
                 return node
         return None
 
+    def find_all_by_prefix(self, label):
+        nodes = []
+        for node in self.svg.iter():
+            node_label = node.get('inkscape:label')
+            if isinstance(node_label, str) and node_label.startswith(label):
+                nodes.append(node)
+        return nodes
+
     def select_area(self):
         os.environ["SELF_CALL"] = "true"
 
         svg = inkex.base.SvgOutputMixin.get_template(width=0, height=0).getroot()
-        g = inkex.Group(*map(clone_node, list(self.svg.selection.values())))
+        g = inkex.Group(*map(lambda n: n.copy(), list(self.svg.selection.values())))
         g.set('id', DEFAULT_GROUP_ID)
         svg.append(g)
         infile = os.path.join(f'in-{randomword(6)}.svg')
@@ -123,67 +124,53 @@ class TaoChuKyExtension(inkex.EffectExtension, inkex.base.TempDirMixin):
         new_rect.style ={'fill' : 'none', 'stroke' : 'none'}
         new_rect.set('label', FRAME_NODE_ID)
         g.append(new_rect)
-
-        if self.options.export_type == 'demo':
-            bg = self.find_by_label('demo-background')
-            if bg != None:
-                padding = 10
-                bg_copy = bg.copy()
-                bg_copy.set('x', x + w0/2 - w/2 - padding)
-                bg_copy.set('y', y + h0/2 - h/2 - padding)
-                bg_copy.set('width', w + padding * 2)
-                bg_copy.set('height', h + padding * 2)
-                g.insert(0, bg_copy)
-            self.export_demo(g)
-        else:
-            self.export_final(g)
-    
-    def export_demo(self, g):
-        os.environ["SELF_CALL"] = "true"
-        opt = self.options
-        filename = os.path.join(self.svg_path(), opt.filename)
-        colors = [('den-demo', False)]
-        self.do_export(g, colors, opt.demo_dpi, filename, True)
+        self.export_final(g)
 
     def export_final(self, g):
         os.environ["SELF_CALL"] = "true"
         opt = self.options
         filename = os.path.join(self.svg_path(), opt.filename)
-        colors = [('mauden', '000000'), ('mautrang', 'f2f2f2'), ('maugold', 'ffd42a')]
-        self.do_export(g, colors, opt.dpi, filename)
-    
-    def gen_demo_export_action(self, id, filename):
-        return f"export-id:{id};export-filename:{filename}-{id}.png;export-do;"
+        self.do_export(g, opt.dpi, filename)
     
     def gen_export_action(self, id, filename):
         return f"export-id:{id};export-filename:{filename}-{id}.png;export-id-only;export-do"
 
-    def do_export(self, node, colors, dpi, filename, demo=False):
+    def do_export(self, node, dpi, filename):
+        my_colors = self.find_all_by_prefix('color:')
         start = time.time()
         self.nomalize_width(node)
 
         svg = inkex.base.SvgOutputMixin.get_template(width=0, height=0).getroot()
         svg.append(self.svg.defs.copy())
         ids = []
-        for cid, color in colors:
+        for color in my_colors:
+            label = color.get('inkscape:label').split(':').pop()
             gc = node.copy()
-            gc.set('id', cid)
-            if color:
-                self.set_fill(gc, f'#{color}')
+            gc.set('id', label)
+            self.copy_style(color, gc)
             svg.append(gc)
-            ids.append(cid)
+            ids.append(label)
 
         tmpfile = os.path.join(f'out-{randomword(4)}.svg')
         inkex.command.write_svg(svg, tmpfile)
         exports = []
-        if demo:
-            exports = list(map(lambda id: self.gen_demo_export_action(id, filename), ids))
-        else:
-            exports = list(map(lambda id: self.gen_export_action(id, filename), ids))
+        exports = list(map(lambda id: self.gen_export_action(id, filename), ids))
         actions = [f"export-dpi:{dpi}"] + exports
         res = inkex.command.inkscape(tmpfile, actions=";".join(actions))
         os.remove(tmpfile)
         logger.debug(f"[Time][Export]: {time.time() - start }")
+
+    def copy_style(self, from_node, to_node):
+        style = from_node.get('style')
+        logger.debug(style)
+        elements = [
+                element
+                for element in to_node.iter()
+                if isinstance(element, (inkex.base.IBaseElement, str))
+        ]
+        for child in elements:
+            if child.get('label') != FRAME_NODE_ID:
+                child.set('style', style)
 
     def set_fill(self, node, color):
         elements = [
